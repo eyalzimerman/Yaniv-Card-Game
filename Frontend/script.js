517235b1544ad511a75da67f3679f76d92ac2e50
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-02-25 20:42:06 +0200

    added function that shuffle cards

--- Frontend/script.js
+++ Frontend/script.js
@@ -87,3 +87,17 @@ function getDeck() {
 }
 
 const allDeck = getDeck();
+
+// shuffle array of cards
+function shuffle(deck) {
+  // for 1000 turns
+  // switch the values of two random cards
+  for (let i = 0; i < 1000; i++) {
+    let location1 = Math.floor(Math.random() * deck.length);
+    let location2 = Math.floor(Math.random() * deck.length);
+    let tmp = deck[location1];
+
+    deck[location1] = deck[location2];
+    deck[location2] = tmp;
+  }
+}
